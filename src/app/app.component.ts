f240f18866093937f966898d2ad0184f3bd596cb
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2018-07-22 18:13:22 -0500

    mapa born

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import XYZ from 'ol/source/XYZ';
+import {TuitsService } from "./services/tuits.service";
+import { Tuit } from './models/tuit';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  map: Map;
+  source: XYZ;
+  layer: TileLayer;
+  view: View;
+  tuits: Tuit[];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          })
+        })
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 3
+      })
+    });
+  }
 }
